435e50d506ebe5b67b6acfdd5c0e344b26df5a1f
tanuj98 <tanuj98@gmail.com>
2019-10-04 18:18:06 -0500

    Added file fetch

--- src/main/java/com/jsoncomparator/demo/JsonController.java
+++ src/main/java/com/jsoncomparator/demo/JsonController.java
@@ -2,6 +2,7 @@ package com.jsoncomparator.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,17 @@ public class JsonController {
     @ResponseBody
     public List<String> getBucketList() {
         return gateway.getDirectories();
+    }
+
+    @GetMapping(path = "/getFile/{bucket}/{file}")
+    @ResponseBody
+    public String response(@PathVariable String bucket, @PathVariable String file) {
+        try {
+            System.out.println(bucket.trim());
+            System.out.println(file.trim());
+            return gateway.getJson(bucket, file);
+        } catch (Exception e) {
+            return e.getMessage();
+        }
     }
 }
