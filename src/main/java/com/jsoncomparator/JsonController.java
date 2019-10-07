8b379fc6173dfaebb08dae8ababb20b88ec6b2c4
Vijit Dhingra <vijitdhingra@gmail.com>
2019-10-06 20:05:48 -0500

    Revert "Changing request structure for getFile"
    This reverts commit a8a5446c76122bf25d8a2c3c58c596cbaaa92cd9.
    okay

--- src/main/java/com/jsoncomparator/JsonController.java
+++ src/main/java/com/jsoncomparator/JsonController.java
@@ -2,7 +2,10 @@ package com.jsoncomparator;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -18,9 +21,9 @@ public class JsonController {
         return gateway.getDirectories();
     }
 
-    @GetMapping(path = "/getFile")
+    @GetMapping(path = "/getFile/{bucket}/{file}")
     @ResponseBody
-    public String response(@RequestParam(name = "bucket") String bucket, @RequestParam(name = "file") String file) {
+    public String response(@PathVariable String bucket, @PathVariable String file) {
         try {
             log.info(bucket.trim());
             log.info(file.trim());
